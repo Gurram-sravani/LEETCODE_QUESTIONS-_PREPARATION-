a5881631e94d372f0614298aa883119830ae0077
Sravani Gurram <48584585+Gurram-sravani@users.noreply.github.com>
2026-04-26 15:47:28 -0700

    Update Max Number of K-Sum Pairs.py

--- Max Number of K-Sum Pairs.py
+++ Max Number of K-Sum Pairs.py
@@ -17,3 +17,11 @@ class Solution:
         return cnt
 # Time Complexity: O(n) creating Hash_map + O(n) checking n in nums
 # Space Complexity : O(n) hash_map 
+-- This is two pass solution - we are doing extra work here - see and understand example below.
+nums=[1,1,1,1]
+for n in nums:
+Iteration 1: current=1,complement=1 => (1,1) so the count becomes 2 in hashmap
+Iteration 2: current=1,complement=1 => (1,1) so the count becomes 0 in hashmap 
+Iteration 3: loops through the third element 
+Iteration 4: loops through 4th element
+Half your loop is useless....
