94022b7225505ff04996b792a7f68fd74e9b0eaa
Sravani Gurram <48584585+Gurram-sravani@users.noreply.github.com>
2026-05-01 12:17:20 -0700

    Create Add Two number sin LinkedList.py

---
+++ Add Two number sin LinkedList.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry=0
+        dummy=ListNode(-1)
+        curr=dummy
+        t1=l1
+        t2=l2
+        node_sum=0
+        while t1 or t2 or carry:
+            node_sum=carry
+            if t1:
+                node_sum=node_sum+t1.val
+                t1=t1.next
+            if t2:
+                node_sum=node_sum+t2.val
+                t2=t2.next
+            new_node=node_sum%10
+            carry=node_sum//10
+            curr.next=ListNode(new_node)
+            curr=curr.next
+        return dummy.next
