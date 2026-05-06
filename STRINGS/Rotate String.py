e9bac49ad3824570545f298b3bde3266e0409eb3
Sravani Gurram <48584585+Gurram-sravani@users.noreply.github.com>
2026-05-06 10:53:52 -0700

    Create Rotate String.py

---
+++ STRINGS/Rotate String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s)!=len(goal):
+            return False
+        new_str=s+s   # O(2n)
+        if goal in new_str:       # new_str.find(goal) != -1  ----> O(2n*n) =>O(n^2)
+            return True
+        else:
+            return False
+# Time Complexity : O(n) positions×O(n) comparisons each=O(n^2)
+# space Complexity1: O(2n)
+
